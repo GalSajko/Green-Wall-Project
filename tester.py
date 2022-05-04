22e56bc7f6f8bbf45d62d7c696ab0549df5864f1
GalSajko <gal.sajko@gmail.com>
2022-05-04 13:46:56 +0200

    Walking demo.

--- tester.py
+++ tester.py
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     spiderGoalPose = [0.4, 1.0, 0.15, 0, 0, 0]
 
 
-    # velocityController.walk(spiderStartPose, spiderGoalPose)
+    velocityController.walk(spiderStartPose, spiderGoalPose)
 
-    path = pathPlanner.calculateSpiderBodyPath(spiderStartPose[:2], spiderGoalPose[:2], 0.05)
-    bestParams = [0.2 , 0.4, 0.4]
-    selectedPins = pathPlanner.calculateSpiderLegsPositionsFF(path, bestParams)
-    plotter.plotSpiderMovement(path, selectedPins)
+    # path = pathPlanner.calculateSpiderBodyPath(spiderStartPose[:2], spiderGoalPose[:2], 0.05)
+    # bestParams = [0.2 , 0.4, 0.4]
+    # selectedPins = pathPlanner.calculateSpiderLegsPositionsFF(path, bestParams)
+    # plotter.plotSpiderMovement(path, selectedPins)
 
 
     
