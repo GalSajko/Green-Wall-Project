184067110a2ceb05c53905ece960d8c4129f0346
GalSajko <gal.sajko@gmail.com>
2022-05-13 08:53:20 +0200

    New dimensions of legs.

--- tester.py
+++ tester.py
@@ -21,39 +21,11 @@ if __name__ == "__main__":
     trajPlanner = planning.TrajectoryPlanner()
     kinematics = calculations.Kinematics()
 
-    motors = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [41, 42, 43], [51, 52, 53]]
-    motorDriver = dynamixel.MotorDriver(motors)
 
     spiderStartPose = [0.4, 0.33, spider.LYING_HEIGHT, 0]
     spiderGoalPose = [0.4, 1, spider.WALKING_HEIGHT, 0]
-    testPose = [0.4, 0.33, spider.WALKING_HEIGHT, 0]
-    secondPose = [0.4, 0.33, 0.3, 0]
-    thirdPose = [0.4, 0.45, 0.3, 0]
 
-    path, pins = pathPlanner.calculateWalkingMovesFF(spiderStartPose, spiderGoalPose)
-
-    p0 = pins[0][0]
-    p1 = pins[0][1]
-    p2 = pins[0][2]
-    p3 = pins[0][3]
-    p4 = pins[0][4]
-
-    p = pins[0]
-    legs = [0, 2, 3]
-    legsLocalPositions = [motorDriver.readLegPosition(leg) for leg in legs]
-
-    position = kinematics.platformDirectKinematics(legs, p[legs], legsLocalPositions)
-    print(position)
-
-    # velocityController.movePlatformWrapper(spiderStartPose, testPose, 5)
-    # velocityController.moveLegsWrapper([0], [[p0[0], p0[1], p0[2]]], thirdPose, [3])
-
-    # position = kinematics.platformDirectKinematics(legs, p[legs], legsLocalPositions)
-    # print(position)
-
-    # while True:
-    #     print(motorDriver.readLegPosition(0))
-
+    velocityController.walk(spiderStartPose, spiderGoalPose)
 
 
 
