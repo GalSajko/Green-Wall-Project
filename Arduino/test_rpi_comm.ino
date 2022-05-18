f8f34c3aaee7eb905fa79b0bc4f12cfd9f9031c3
GalSajko <gal.sajko@gmail.com>
2022-05-18 10:45:10 +0200

    Reading switch value.

--- Arduino/test_rpi_comm.ino
+++ Arduino/test_rpi_comm.ino
@@ -2,26 +2,33 @@
 
 Servo myServo;
 
-//Constanst for pin connections etc.
-int servo1Pin = 8;
-float maxStroke = 30;
-float minStroke = 0;
-float openStroke = 7;
-float closedStroke = 20;
-float desiredLength = 0;
+// Commands for controlling a gripper.
+const String openCommand = "o";
+const String closeCommand = "c";
 
-float LimitStroke(float strokeDesired){
+// Constants for pin connections etc.
+const int servo1Pin = 8;
+const int switchPin = 9;
+const float maxStroke = 30;
+const float minStroke = 0;
+const float openStroke = 10;
+const float closedStroke = 20;
+
+float desiredLength;
+int switchValue;
+
+float limitStroke(float strokeDesired){
   if (strokeDesired > maxStroke){
     strokeDesired = maxStroke;
   }
-  if (strokeDesired < minStroke){
+  else if (strokeDesired < minStroke){
     strokeDesired = minStroke;
   }
   return strokeDesired;
 }
 
-void SetStrokeMm(float strokeDesired){
-  strokeDesired = LimitStroke(strokeDesired);
+void setStrokeMm(float strokeDesired){
+  strokeDesired = limitStroke(strokeDesired);
   int usec = 1000 + strokeDesired * (2000 - 1000) / maxStroke;
   myServo.writeMicroseconds(usec);
 }
@@ -31,21 +38,26 @@ void setup() {
   Serial.begin(9600);
   myServo.writeMicroseconds(1500);
   myServo.attach(servo1Pin);
+
+  pinMode(switchPin, INPUT);
 }
 
 void loop() {
+  switchValue = digitalRead()
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0){
     String data = Serial.readStringUntil('\n');
     desiredLength = data.toFloat();
-    SetStrokeMm(desiredLength);
-    if (data == "o"){
-      SetStrokeMm(10);
+
+    setStrokeMm(desiredLength);
+
+    if (data == openCommand){
+      setStrokeMm(openStroke);
       delay(2000);
       Serial.println("Gripper opened.");
     }
-    else if (data == "c"){
-      SetStrokeMm(20);
+    else if (data == closeCommand){
+      setStrokeMm(closedStroke);
       delay(2000);
       Serial.println("Gripper closed.");
     }
