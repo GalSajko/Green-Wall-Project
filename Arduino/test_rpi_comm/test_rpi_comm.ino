1291b26e752630fa7de56d9c0749a7585bcfec53
GalSajko <gal.sajko@gmail.com>
2022-05-19 13:25:02 +0200

    Arduino gripper controll.

--- Arduino/test_rpi_comm/test_rpi_comm.ino
+++ Arduino/test_rpi_comm/test_rpi_comm.ino
@@ -50,43 +50,33 @@ void setup() {
 
 void loop() {
   
-    if (Serial.available() > 0){
-      String data = Serial.readStringUntil('\n');
-      Serial.print("Received: ");
-      Serial.println(data);
-    }
-
-
-
-
-
   //  switchValue = digitalRead(switchPin);
   //  Serial.println(switchValue);
-  // if (Serial.available() > 0){
-  //   String data = Serial.readStringUntil('\n');
+   if (Serial.available() > 0){
+     String data = Serial.readStringUntil('\n');
 
-  //   if (data == openCommand){
-  //     setStrokeMm(openStroke);
-  //     delay(2000);
-  //     servoValue = analogRead(servo1FeedbackPin);
-  //     if (servoValue > openThreshold){
-  //       Serial.println("1");
-  //     }
-  //     else{
-  //       Serial.println("0");
-  //     }
-  //   }
-  //   else if (data == closeCommand){
-  //     setStrokeMm(closedStroke);
-  //     delay(2000);
-  //     servoValue = analogRead(servo1FeedbackPin);
-  //     if (servoValue < closeThreshold){
-  //       Serial.println("1");
-  //     }
-  //     else{
-  //       Serial.println("0");
-  //     }
-  //   }
+     if (data == openCommand){
+       setStrokeMm(openStroke);
+       delay(2000);
+       servoValue = analogRead(servo1FeedbackPin);
+       if (servoValue > openThreshold){
+         Serial.println("1");
+       }
+       else{
+         Serial.println("0");
+       }
+     }
+     else if (data == closeCommand){
+       setStrokeMm(closedStroke);
+       delay(2000);
+       servoValue = analogRead(servo1FeedbackPin);
+       if (servoValue < closeThreshold){
+         Serial.println("1");
+       }
+       else{
+         Serial.println("0");
+       }
+     }
     
-  // }
+   }
 }
