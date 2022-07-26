5d490513789d0eccd0729ed968450abe43259c99
GalSajko <gal.sajko@gmail.com>
2022-07-26 15:47:36 +0200

    Testing on 0.3m height.

--- tester.py
+++ tester.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     wall = env.Wall('squared')
     pathPlanner = planning.PathPlanner(0.05, 0.1)
     plotter = simulaton.Plotter('squared')
-    start = [0.6, 0.3, 0.03, 0.0]
-    goal = [1.3, 1.0, 0.03, 0.0]
+    start = [0.6, 0.3, 0.3, 0.0]
+    goal = [1.3, 1.0, 0.3, 0.0]
     path = pathPlanner.calculateSpiderBodyPath(start, goal)
 
     selectedPins = pathPlanner.calculateSelectedPins(path)
