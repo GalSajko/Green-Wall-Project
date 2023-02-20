45707fc717007c6017dc0f9ca1326244ba212109
jurck222 <jurck222@gmail.com>
2023-02-20 06:47:24 +0100

    fixed comments and suggestions

--- environment/comunication.py
+++ environment/comunication.py
@@ -5,40 +5,28 @@ sys.path.append('..')
 import threadmanager
 import config
 import threading
+import config
 
 class CommunicationWithServer:
     def __init__(self) :
         self.threadManager = threadmanager.CustomThread()
-        # Ime lahko poenostavis preprosto v locker - statesObjectsLocker sem poimenoval Lock() objekt v app.py samo zato, ker lock-a vse spremenljivke stanja robota (kote, pozicije nog, tokove, ...).
-        self.statesObjectsLocker = threading.Lock()
+        self.sensorPosition = []
+        self.locker = threading.Lock()
 
-    # V celotnem programu je veliko 'pozicij', npr. pozicija pajka, pozicija pajkove noge, pozicija pina, pozicija roze itd. Izberi taksno ime, da se bo tocno vedelo, da gre za pozicijo roze.  
-    def updatePositionData(self):
+    def updateSensorPositionData(self):
         """Comunication procedure, creates a thread that continuously sends GET requests to the server and updates the data variable with values from the server.
         """
-        def updatingPositionData(killEvent):
-            # Uporabi while True (samo zaradi konsistentnosti, nic drugega).
-            while 1:
+        def updatingSensorPositionData(killEvent):
+            while True:
                 try:
-                    # Definiraj ta string naslova kot konstanto. Ce bo do te konstante moral dostopati se kak drug objekt, ki ne bo inicializiral nove instance CommunicationWithServer objekta, potem definiraj
-                    # konstanto v config.py file-u. Ce ne, jo lahko definiras tukaj, na zacetku __init__ (definicija konstant pride pred ostalimi definicijami + poimenuj konstanto v stilu TO_JE_KONSTANTA).
-                    request = requests.get('http://192.168.1.20:5000/zalij')
-                    # Presledki pred in po operatorju.
-                    if len(request._content.decode())!=0:
-                        # 1.) Tukaj je potrebno uporabiti lock objekt (imas dve moznosti - self.statesObjectsLocker.aquire() + self.statesObjectsLocker.release() 
-                        # ali pa (kar je bolj 'sodobno' in se naceloma uporablja) with self.statesObjectsLocker: ...). 
-                        # Kot je program sedaj zastavljen, ta thread neprestano zapisuje v self.data, v app.py pa se na vsake toliko ta ista spremenljivka (oz. vsebina istega naslova v pomnilniku) prebere.
-                        # Da ne pride do nepredvidljivih stvari v primeru, da se ob istem trenutku v enem threadu zapise, v drugem pa prebere vsebino istega naslova, se uporabi locker, ki 'zaklene' dostop do tega naslova.
-                        # 2.) Sam sem vse deklaracije spremenljivk objekta (self.) dajal v __init__ metodo da se takoj ve, katere so spremenljivke objekta in so dostopne navzven. Ce ob deklaraciji nima vrednosti, 
-                        # ji lahko das vrednost None.
-                        # 3.) Ime self.data ne nakazuje, da sta v tej spremenljivki shranjena podatka o senzorju in vrstici (na koncu bo to podatek o dejanski x, y, z poziciji rastline, ki potrebuje zalivanje).
-                        self.data=json.loads(request._content)
+                    request = requests.get(config.GET_SENSOR_POSITION_ADDR)
+                    if len(request._content.decode()) != 0:
+                        with self.locker:
+                            self.sensorPosition=json.loads(request._content)
                 # Tukaj lovis vse mogoce exceptione, do katerih lahko pride pri klicih get(), decode(), len() in loads() metod. 
                 # Ne pravim, da to ni OK, samo mogoce si vzami par minut in premisli, ce bi kateri od teh zahteval kaksen poseben handling.
                 except:
                     print("will retry in a second")
-                # Timeout tukaj je lahko brez problema 30s ali se vec.
-                if killEvent.wait(timeout = 2): 
+                if killEvent.wait(timeout = 30): 
                     break
-        # Sam thread ne rabi biti spremenljivka objekta (self.). Navzven mora biti dostopen samo event, ki ubije thread (po c-jevsko: 'public' mora biti samo event, da lahko iz drugje ubijes ta thread).
-        self.updatingDataThread, self.updatingDataThreadKillEvent = self.threadManager.run(updatingPositionData, config.UPDATE_DATA_THREAD_NAME, False, True)
+        self.updatingDataThread, self.updatingDataThreadKillEvent = self.threadManager.run(updatingSensorPositionData, config.UPDATE_DATA_THREAD_NAME, False, True)
--- environment/server.py
+++ environment/server.py
@@ -1,57 +1,54 @@
 # Pri nekaterih funkcijah manjka dokumentacija.
 from flask import Flask, request, jsonify, render_template
+import math
 import wall
 
 values = []
 minVal = []
+visualisationValues = [[[]]]
 
-# Presledki pred in po =.
-visualisationValues=[[[]]]
 app = Flask(__name__)
-app.secret_key=b'asdasgfascajv'
-# Prazna vrstica.
+app.secret_key = b'asdasgfascajv'
+
 def updateVisualisationValues(data, ip):
-    # Ta prazna vrstica je odvec.
-    arduino = 0
-    
-    # Vse IP naslove, ki bodo fiksno doloceni (arduino, server, ...) shrani kot konstante, najbolje v config.py.
-    # Pomisli tudi na to, da bos na koncu imel 6 Arduinov. Razmisli, kako bi mappal IP naslov v id arduinota, brez uporabe 6-ih if stavkov.
-    if ip == "192.168.1.10":       
-        arduino = 1
-    elif ip == "192.168.1.11":
-        arduino = 2
-        # Prazna vrstica (med if-elif in try-except bloki).
+    """Function inserts value 1 to for every sensor currently connected to the arduino.
+
+    Args:
+        data (JSON): Data from arduino in JSON format.
+        ip (String): Arduino address.
+
+    Returns:
+        list: List with sensor locations.
+    """
+    arduino = int(ip[len(ip)-1])
     try:
         visualisationValues.pop(arduino)
-    # Med try-except ni prazne vrstice - vse je del enega try-except bloka.
     except:
         print("New arduino joined")
-    # Prazna vrstica, presledek za vejico.
     visualisationValues.insert(arduino,[])
-    # range(0, 5) je enako kot samo range(5) - velja tudi za spodaj.
-    for i in range(0, 5):
-        visualisationValues[arduino].insert(i,[])
-        # Pazi - range(x, y) vkljucuje x, y pa ne (probaj kaj ti sprinta koda for i in range(5, 10): print(i))
+    for i in range(6):
+        try:
+            visualisationValues[arduino].insert(i, [])
+        except IndexError:
+            continue
         for j in range(53, 60):
             try:
                 # Glej komentar spodaj glede t.i. 'magic number-jev'. Naslove senzorjev lahko shranis v array in ga definiras kot konstanto (npr. WALL_SENSORS_IDS = [54, 55, 56, 57]).
                 if data["vrstica" + str(i)]["senzor" + str(j)]["id"] == 54:
-                    # Presledki za vejico (argumenti insert() funkcije).
-                    visualisationValues[arduino][i].insert(0,1)
+                    visualisationValues[arduino][i].insert(0, 1)
                 elif data["vrstica" + str(i)]["senzor" + str(j)]["id"] == 55:
                     visualisationValues[arduino][i].insert(1,1)
                 elif data["vrstica" + str(i)]["senzor" + str(j)]["id"] == 56:
                     visualisationValues[arduino][i].insert(2,1)
                 elif data["vrstica" + str(i)]["senzor" + str(j)]["id"] == 57:
-                   visualisationValues[arduino][i].insert(3,1) 
+                   visualisationValues[arduino][i].insert(3, 1) 
             except:
                 continue
-    # Vem, da si se v fazi razvijanja. Ko bos koncal pa pobrisi vse te zakomentirane vrstice kode.
-    #print(visualisationValues)
+    
+    print(visualisationValues)
     return visualisationValues
     #print(dataJson.data)
             
-# Ena prazna vrstica med metodami in nasploh ostalimi deli kode je dovolj - velja isto za spodaj.   
 def getMin():
     """
     Finds the lowest sensor value from the list of lowest values that were gathered from arduinos since the last call of this function and resets said list.
@@ -79,24 +76,16 @@ def parseData(data, ip):
     Returns:
         list: Information about the sensor with the lowest value from a single arduino.
     """
-    # Je ta spremenljivka kje v uporabi?
-    parsed = {}
-    #dataJson = data
-    #print(dataJson)
     y = 0
     x = 0
-    # Tukaj sklepas, da nobena vrednost nikoli ne bo vecja od 1347865476. Kaj pa ce slucajno bo? Od Python 3.5 naprej obstaja objekt math.inf (aka infinity), za 
-    # katerega velja, da je vecji od kateregakoli stevila.
-    minCap = 1347865476
+    minCap = math.inf
     currCap = 0
-    parsed["ip"] = ip
- 
     # 1.) Definiraj te stevilke kot konstante. Nasploh se izogibaj uporabi t.i. 'magic number', 'magic string' itd. Vse stevilke, stringe itd.,
     # ki imajo nek pomen (konkretno tukaj gre za stevilo vrstic na panelu in pa najmanjsi in najvecji mozen naslov senzorja) shrani v konstanto. Ce se bo enkrat v prihodnosti slucajno spremenila
     # konfiguracija panela, ali pa se bodo spremenili ID-ji senzorjev, bomo vsi tocno vedeli, kje je potrebno to spremeniti in ne bomo rabili iti skoz celo kodo. Prav tako, bos mogoce moral
     # te vrednosti uporabiti se kje v kodi (oz si jih ze, zgoraj v kodi).
     # 2.) Enako kot zgoraj, na hitro premisli, kako bi lahko poiskal minimum in pripadajoce x, y, IP vrednosti brez uporabe dvojne for zanke.  
-    for i in range(0, 5):
+    for i in range(6):
         for j in range(53, 60):
             try:
                 currCap = data["vrstica" + str(i)]["senzor" + str(j)]["cap"]
@@ -107,8 +96,6 @@ def parseData(data, ip):
                 y = i
                 x = data["vrstica" + str(i)]["senzor" + str(j)]["id"]
     values.append([ip, y, x, minCap])
-    # Je return tukaj potreben?
-    return [ip, y, x, minCap]
 
 @app.route('/zalij',methods=["GET"])
 def getVal():
@@ -117,9 +104,6 @@ def getVal():
 @app.route('/ping',methods=["GET"])
 def pingPong():
     return jsonify('pong!'), 200, {"Access-Control-Allow-Origin": "*"}
-
-
-
 
 @app.route('/data', methods=['POST', 'GET'])
 def handleData():
@@ -130,15 +114,9 @@ def handleData():
     """
     ip = request.remote_addr
     data = request.get_json()
-    
     dataJson = updateVisualisationValues(data, ip)
-
-    
-    #print(data)
     parseData(data, ip)
-    
-    # Presledek pred in po =.
-    return render_template('index.html', datas=dataJson)
+    return render_template('index.html', datas = dataJson)
 
 if __name__ == '__main__':
     app.run(host = '192.168.1.20', port = 5000, debug = True)
