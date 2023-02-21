Fixed bug in spider pose calculation, manual correction.

controllers.py
@@ -39,6 +39,8 @@ class VelocityController:
         self.isImpedanceMode = False
         self.impedanceLegId = None
         self.impedanceDirection = np.zeros(3, dtype = np.float32)
+        self.maxAllowedForce = 3.5
+        self.velocityFactor = 0.1
 
         time.sleep(1)
 
@@ -66,7 +68,7 @@ class VelocityController:
                 self.lastLegsPositions[forceModeLegsIds] = xA[forceModeLegsIds]
         
         if isImpedanceMode:
-            xDd[impedanceLegId] = 0.1 * impedanceDirection * int(np.linalg.norm(fA[impedanceLegId]) < 3.0)
+            xDd[impedanceLegId] = self.velocityFactor * impedanceDirection * int(np.linalg.norm(fA[impedanceLegId]) < self.maxAllowedForce)
             with self.locker:
                 self.lastLegsPositions[impedanceLegId] = xA[impedanceLegId]
 

environment/spider.py
@@ -93,7 +93,8 @@ VECTORS_TO_COG_SEGMENT = np.array([
     [0.032, 0.19, 0.158],
     [0.032, 0.19, 0.158]], dtype = np.float32)
 # Leg limit to avoid singularity.
-LEG_LENGTH_LIMIT = 0.6
+LEG_LENGTH_MAX_LIMIT = 0.6
+LEG_LENGTH_MIN_LIMIT = 0.32
 # Angles between legs, looking from spiders origin.
 ANGLE_BETWEEN_LEGS = np.radians(360.0 / NUMBER_OF_LEGS)
 # Positions of leg anchors on spiders platform, given in spiders origin - matching the actual legs order on spider.

tester.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     spider = app.App()
     time.sleep(1)
     # input("ENTER")
-    # spider.motorsVelocityController.startForceMode([3], [np.array([0.0, 0.0, 0.0])])
+    # spider.motorsVelocityController.startForceMode([2], [np.array([0.0, 0.0, 0.0])])
     spider.spiderStatesManager(config.WORKING_STATE, (np.array([0.6, 0.4, 0.3, 0.0]), ))
 